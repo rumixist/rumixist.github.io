b201a737da8044305622ca9239bf8946b1f584de
rumixist <133505669+rumixist@users.noreply.github.com>
2025-08-19 16:27:38 +0300

    Delete logout.js

--- logout.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const logoutButton = document.getElementById('logoutButton');
-
-    logoutButton.addEventListener('click', function () {
-        // Sadece çıkış yapıldığında token'ı sil
-        localStorage.removeItem("token");
-        window.location.href = "login.html"; // Kullanıcıyı login sayfasına yönlendir
-    });
-});
