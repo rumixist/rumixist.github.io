74fb21f6d31831821f14ef9c566d7cb5056e69b1
rumixist <133505669+rumixist@users.noreply.github.com>
2025-08-19 16:29:39 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("mousemove", (e) => {
-    const customCursor = document.querySelector(".custom-cursor");
-    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    
-    customCursor.style.left = e.clientX + "px";
-    customCursor.style.top = e.clientY + scrollTop + "px";
-});
